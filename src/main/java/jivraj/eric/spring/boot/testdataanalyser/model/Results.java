acb62a2dfe1eb941cec54f130634418559e739f7
Eric Jivraj <Eric.Jivraj@pcmsgroup.com>
2020-03-02 20:31:16 +0000

    Removed unnecessary import

--- src/main/java/jivraj/eric/spring/boot/testdataanalyser/model/Results.java
+++ src/main/java/jivraj/eric/spring/boot/testdataanalyser/model/Results.java
@@ -1,7 +1,6 @@
 package jivraj.eric.spring.boot.testdataanalyser.model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
